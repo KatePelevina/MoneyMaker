27d0af472a9a732e82eb12bc8a9faf30bf5a145d
KatePelevina <64221065+KatePelevina@users.noreply.github.com>
2023-11-01 21:48:33 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-var modal = document.getElementById("my_modal");
-var btn = document.getElementById("btn_modal_window");
-var span = document.getElementsByClassName("close_modal_window")[0];
+// var modal = document.getElementById("my_modal");
+// var btn = document.getElementById("btn_modal_window");
+// var span = document.getElementsByClassName("close_modal_window")[0];
 
-btn.onclick = function () {
-   modal.style.display = "block";
-}
+// btn.onclick = function () {
+//    modal.style.display = "block";
+// }
 
-span.onclick = function () {
-   modal.style.display = "none";
-}
+// span.onclick = function () {
+//    modal.style.display = "none";
+// }
 
-window.onclick = function (event) {
-   if (event.target == modal) {
-       modal.style.display = "none";
-   }
-}
+// window.onclick = function (event) {
+//    if (event.target == modal) {
+//        modal.style.display = "none";
+//    }
+// }
 
 $(document).ready(function () {
    $(".klundesaga-title").click(function (e) {
